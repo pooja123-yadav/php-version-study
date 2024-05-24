7165c931ea5498955ec68a582537f994d9478c30
pooja123-yadav <85959414+pooja123-yadav@users.noreply.github.com>
2024-05-24 23:33:33 +0530

    Update php7-features.php

--- php7-features.php
+++ php7-features.php
@@ -7,3 +7,13 @@ function upper(?string $str): string
 
 $str = null;
 echo upper($str);
+
+// Starting from PHP 7.0, if a function doesn’t return a value, you use the void type
+
+function dd($data):void
+{
+    echo '<pre>';
+    var_dump($data);
+    echo '</pre>';
+    die;
+}
