722b5f000b51adf29d568e8b2deaa1ca6b442e38
pooja123-yadav <85959414+pooja123-yadav@users.noreply.github.com>
2024-05-24 22:45:32 +0530

    Update php7->php8.php

--- php7->php8.php
+++ php7->php8.php
@@ -1,4 +1,7 @@
 <?php
+/*  
+1. Union Types
+*/
 	class test{
 	  public function abc(string $name, int $age, int $height): int {
 	    return $age;
@@ -18,4 +21,63 @@
 	$res = $obj->abc("pooja",32,5.3);
 	echo $res;
 	
+?>
+
+<?php
+/*  
+2. nullsafe operator Argument
+*/
+<?php
+	// php  7
+$country =  null;
+$data = ["user"=>["address"=>["country"=>"India"]]];
+$session = json_decode(json_encode($data, JSON_PRETTY_PRINT));
+
+$country =  null;
+
+if ($session !== null) {
+  $user = $session->user;
+
+  if ($user !== null) {
+    $address = $user->address;
+ 
+    if ($address !== null) {
+      $country = $address->country;
+    }
+  }
+}
+echo $country;
+
+?>
+
+<?php
+// php 8
+$country =  null;
+$data = ["user"=>["address"=>["country"=>"India"]]];
+$session = json_decode(json_encode($data, JSON_PRETTY_PRINT));
+
+$country = $session?->user?->address?->country;
+echo $country;
+
+?>
+
+
+<?php
+/* constructor property promotion  */
+class BankAccount
+{
+	function __construct(public $accountNumber, public $balance)
+	{}
+}
+$b = new BankAccount(12345,100);
+?>
+
+<?php
+class BankAccount
+{
+	function __construct(public $accountNumber, public $balance){}
+}
+
+$b = new BankAccount(12345,100);
+echo $b->balance;
 ?>
